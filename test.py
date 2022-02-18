25c98672f782a14cb929485563a3aa92886982e4
Omur.Simsek <omursimsek@gmail.com>
2022-02-18 16:30:47 +0300

    sample api endpoint is added  "/api/v1/resources/books/all"

--- test.py
+++ test.py
@@ -1,8 +1,34 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+
+books = [
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1992'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'published': '1975'}
+]
+
+
 @app.route('/')
 def hello_world():
     return 'hello test'
+
+@app.route('/api/v1/resources/books/all', methods=['GET'])
+def api_all():
+    return jsonify(books)
+
 if __name__ == '__main__':
     app.run()
